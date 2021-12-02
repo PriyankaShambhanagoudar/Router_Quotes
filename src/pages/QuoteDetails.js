23fd0f5a358f82dd04277d36855bcc77ee8d7614
React_Teaching_Project <priyassss789@gmail.com>
2021-12-02 12:31:02 +0530

    Getting Creative with Routing Code

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,15 +1,15 @@
-import { useParams, Route } from "react-router";
+import { useParams, Route, Link } from "react-router-dom";
 import { Fragment } from "react/cjs/react.production.min";
 
-import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 import Comments from "../components/comments/Comments";
 
 const DUMMY_QUOTES = [
-    { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
-    { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+    { id: "q1", author: "Max", text: "Learning React is fun!" },
+    { id: "q2", author: "Maximilian", text: "Learning React is great!" },
 ];
-
 
 const QuoteDetail = () => {
     const params = useParams();
@@ -23,9 +23,17 @@ const QuoteDetail = () => {
     return (
         <Fragment>
             <HighlightedQuote text={quote.text} author={quote.author} />
+            <Route path={`/quotes/${params.quoteId}`} exact>
+                <div className='centered'>
+                    <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}> Load comments </Link>
+                </div>
+            </Route>
+
+
             <Route path={`/quotes/${params.quoteId}/comments`}>
                 <Comments />
             </Route>
+
         </Fragment>
     );
 };
